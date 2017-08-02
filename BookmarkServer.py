757e1e6d0845e48e2ba09d78ea0901e5b163e5a1
Nimit Bhargava <nimitbhargava@gmail.com>
2017-08-02 10:24:36 +0530

    303 redirect to the form after saving a newly submitted URI

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -1,51 +1,10 @@
 #!/usr/bin/env python3
-#
-# A *bookmark server* or URI shortener that maintains a mapping (dictionary)
-# between short names and long URIs, checking that each new URI added to the
-# mapping actually works (i.e. returns a 200 OK).
-#
-# This server is intended to serve three kinds of requests:
-#
-#   * A GET request to the / (root) path.  The server returns a form allowing
-#     the user to submit a new name/URI pairing.  The form also includes a
-#     listing of all the known pairings.
-#   * A POST request containing "longuri" and "shortname" fields.  The server
-#     checks that the URI is valid (by requesting it), and if so, stores the
-#     mapping from shortname to longuri in its dictionary.  The server then
-#     redirects back to the root path.
-#   * A GET request whose path contains a short name.  The server looks up
-#     that short name in its dictionary and redirects to the corresponding
-#     long URI.
-#
-# Your job in this exercise is to finish the server code.
-#
-# Here are the steps you need to complete:
-#
-# 1. Write the CheckURI function, which takes a URI and returns True if a
-#    request to that URI returns a 200 OK, and False otherwise.
-#
-# 2. Write the code inside do_GET that sends a 303 redirect to a known name.
-#
-# 3. Write the code inside do_POST that sends a 400 error if the form fields
-#    are missing.
-#
-# 4. Write the code inside do_POST that sends a 303 redirect to the form
-#    after saving a newly submitted URI.
-#
-# 5. Write the code inside do_POST that sends a 404 error if a URI is not
-#    successfully checked (i.e. if CheckURI returns false).
-#
-# In each step, you'll need to delete a line of code that raises the
-# NotImplementedError exception.  These are there as placeholders in the
-# starter code.
-#
-# After writing each step, restart the server and run test.py to test it.
 
 import http.server
 import requests
 from urllib.parse import unquote, parse_qs
 
-memory = {}
+memory = {} 
 
 form = '''<!DOCTYPE html>
 <title>Bookmark Server</title>
@@ -125,8 +84,10 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             memory[shortname] = longuri
 
             # 4. Serve a redirect to the root page (the form).
-            #    Delete the following line.
-            raise NotImplementedError("Step 4 isn't written yet!")
+            # Send a 303 back to the root page
+            self.send_response(303)  # redirect via GET
+            self.send_header('Location', '/')
+            self.end_headers()
         else:
             # Didn't successfully fetch the long URI.
 
